41a92fce64a248a7b71edd64e523ea36ba2c6464
on a t break <kasrahashemi1@gmail.com>
2025-03-17 18:39:23 -0700

    v0.0.3 bank fix

--- contracts/eosio.bank/include/eosio.bank.hpp
+++ contracts/eosio.bank/include/eosio.bank.hpp
@@ -32,7 +32,7 @@ class [[eosio::contract("eosio.bank")]] bank : public eosio::contract {
 
     [[eosio::action]] void cleanup();
 
-    [[eosio::on_notify("twig.token::transfer")]] void token_deposit(name from, name to, std::vector<asset> tokens, std::string memo);
+    [[eosio::on_notify("eosio.token::transfer")]] void token_deposit(name from, name to, std::vector<asset> tokens, std::string memo);
     [[eosio::action]] void onepoch(uint32_t epoch);
 
 // TABLES
